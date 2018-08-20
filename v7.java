d0efe6b86597341e229f3bf43cc5ab8a3880458d
vinay6465 <42532109+vinay6465@users.noreply.github.com>
2018-08-20 12:25:54 +0530

    Create v7.java

---
+++ v7.java
@@ -0,0 +1,22 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	Scanner sc=new Scanner(System.in);	
+	int i=sc.nextInt();
+	for(int j=0;j<i;j++)
+	{
+		System.out.println("Hello");
+//		{
+//			System.out.print(" ");
+//		}
+	}// your code goes here
+	}
+}
